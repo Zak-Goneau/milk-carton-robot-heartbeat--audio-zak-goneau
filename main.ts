492eeacf00f381d14435f9aefacf2eaea911e107
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-11 10:22:58 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onGesture(Gesture.FreeFall, function () {
     music.playMelody("C D G F E D G C ", 268)
 })
 input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 8; index++) {
         basic.showIcon(IconNames.Heart)
         basic.pause(400)
         basic.showIcon(IconNames.SmallHeart)
