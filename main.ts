ae1c1471eff6ae9f3da96c9677634c9799bb3602
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-13 12:38:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-	
-})
-input.onGesture(Gesture.FreeFall, function () {
     music.playMelody("C D G F E D G C ", 268)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 1; index++) {
+        basic.showNumber(randint(0, 10))
+        basic.pause(500)
+        basic.showIcon(IconNames.Heart)
+    }
 })
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 8; index++) {
@@ -11,7 +15,9 @@ input.onGesture(Gesture.Shake, function () {
         basic.showIcon(IconNames.SmallHeart)
         basic.pause(300)
     }
+    basic.showIcon(IconNames.Heart)
 })
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     pins.setAudioPin(AnalogPin.P0)
 })
