429cc8b8d3765af1ac04d9ceaef0807df3de3bd5
Zak Goneau <118993110+Zak-Goneau@users.noreply.github.com>
2023-01-13 18:51:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
+// When Button A is pressed audio will play
 input.onButtonPressed(Button.A, function () {
     music.playMelody("C D G F E D G C ", 268)
 })
+// When button B is pressed it generates a random number from 0-10, you can play it as a game.
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 1; index++) {
         basic.showNumber(randint(0, 10))
@@ -8,6 +10,7 @@ input.onButtonPressed(Button.B, function () {
         basic.showIcon(IconNames.Heart)
     }
 })
+// When the Micro:bit is shaken the Heart will beat 8 times then go back to normal.
 input.onGesture(Gesture.Shake, function () {
     for (let index = 0; index < 8; index++) {
         basic.showIcon(IconNames.Heart)
@@ -17,7 +20,9 @@ input.onGesture(Gesture.Shake, function () {
     }
     basic.showIcon(IconNames.Heart)
 })
+// When first started it shows a heart.
 basic.showIcon(IconNames.Heart)
+// Pin 0 on the Micro:bit is connected to a speaker.
 basic.forever(function () {
     pins.setAudioPin(AnalogPin.P0)
 })
